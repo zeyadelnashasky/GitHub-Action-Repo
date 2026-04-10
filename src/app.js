8cd5307a57d8360746d3fad7cacbc00dbd44b66b
Zeyad Moustafa Kamel <zeyadmoustafa732@gmail.com>
2026-04-10 16:12:08 +0200

    Make Change on Branch Test

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ function greet(name) {
 module.exports = greet;  // Ensure function is exportable
 // if run directly, print output (useful for debugging)  
   
-if (require.main === module) {
+//if (require.main === module) {
 
-    console.log(greet('World')); 
+// console.log(greet('World')); 
 
-}
+//}
