469c57a4b613c04db9bc48e510c78bf9aeaec1ed
Zeyad Moustafa Kamel <zeyadmoustafa732@gmail.com>
2026-04-10 13:42:58 +0200

    Creating Simple NodeJS app

--- src/test.sh
+++ src/test.sh
@@ -6,11 +6,14 @@ EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
-  echo "✅ Test passed!"
-  exit 0
+echo "✅ Test passed!"
+  
+exit 0
 
 else
 
-  echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
-  exit 1
+echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+  
+exit 1
+
 fi
